e5e4f388808fb95f13b7370e04a894850437eded
GeGuNa <geguna27@gmail.com>
2026-04-29 06:40:10 +0400

    ..

---
+++ api/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../utils/response.php';
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json; charset=utf-8');
+header('X-Content-Type-Options: nosniff');
+header('X-Frame-Options: DENY');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
